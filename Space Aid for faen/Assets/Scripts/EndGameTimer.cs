2e6e8c42a5399f1babd4fdbd80d83796e77a5358
Haabr <haakon_b94@hotmail.com>
2021-11-15 20:09:28 +0100

    update

--- Space Aid for faen/Assets/Scripts/EndGameTimer.cs
+++ Space Aid for faen/Assets/Scripts/EndGameTimer.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 
 public class EndGameTimer : MonoBehaviour
 {
-    int countDownStartValue = 5;
+    int countDownStartValue = 60;
     public Text TimerIU;
     public GameObject PauseMenuUI;
 
